7b54e2a2a6388449e4a19a181a06bd259708b44e
Abdulla Javadov <abdullacavadov@gmail.com>
2026-09-09 00:53:50 +0400

    feat: mount media production detail panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { SettingsHost } from './components/SettingsPage'
+import { MediaProductionPanel } from './components/MediaProductionPanel'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <SettingsHost />
+    <MediaProductionPanel />
   </StrictMode>,
 )
